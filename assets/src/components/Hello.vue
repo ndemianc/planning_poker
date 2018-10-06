16ade8c3e598490692018a660cac2c6d1954adc5
Sergii Demianchuk <sdemian@softserveinc.com>
2018-10-06 23:48:08 +0300

    Initial Commit

---
+++ assets/src/components/Hello.vue
@@ -0,0 +1,24 @@
+<!-- components/Hello.vue -->
+<template>
+  <div class="hello">
+    <h3>Hello from Vue.js!</h3>
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'hello',
+        data() {
+            return {
+                msg: 'Simple example using Phoenix, Vue and Webpack by sdemian'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    p {
+        margin-top: 40px;
+    }
+</style>
---
+++ assets/src/main.js
@@ -0,0 +1,11 @@
+// => main.js
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
